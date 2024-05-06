d6cbe89c1049a684837da529cbbf6d8a1fe358eb
Naveen Kumar <nk24528.nk81@gmail.com>
2024-05-06 13:26:29 +0530

    cors added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const productsRouter = require("./routes/productsRouter");
 const app = express();
 
 app.use(express.json());  
-app.use(express.cors())
+app.use(cors())
 app.use('/users', userRouter)
 app.use('/products', productsRouter)
 
