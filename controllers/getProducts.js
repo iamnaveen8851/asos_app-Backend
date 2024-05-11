5aeded7d4500dbab0e6b37cf7288de63dd511311
Naveen Kumar <nk24528.nk81@gmail.com>
2024-05-11 11:29:01 +0530

    search functionality added

--- controllers/getProducts.js
+++ controllers/getProducts.js
@@ -5,32 +5,39 @@ const getProducts = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 20;
     const skip = (page - 1) * limit;
-    const totalProducts = await productsModel.countDocuments();
-    const totalPages = Math.ceil(totalProducts / limit);
 
-    let query = productsModel.find()
+    let query = productsModel.find();
 
-    if(req.query.sortByPrice){
+    // Search functionality
+    if (req.query.search) {
+      const searchRegex = new RegExp(req.query.search, "i"); // 'i' for case-insensitive search
+      query = query.find({
+        $or: [{ title: searchRegex }, { description: searchRegex }],
+      });
+    }
+
+    // sorting products based on price
+    if (req.query.sortByPrice) {
       const sortByPrice = req.query.sortByPrice.toLowerCase();
-      if(sortByPrice === "asc"){
-        query =  query.sort({price : 1}) // asc order == 1
-      }else if(sortByPrice === "desc"){
-        query = query.sort({price : -1}) // desc order == -1
+      if (sortByPrice === "asc") {
+        query = query.sort({ price: 1 }); // asc order == 1
+      } else if (sortByPrice === "desc") {
+        query = query.sort({ price: -1 }); // desc order == -1
       }
     }
 
+    const totalProducts = await productsModel.countDocuments(query); // Count documents using the same query
+    const totalPages = Math.ceil(totalProducts / limit);
 
-    const products = await query.skip(skip).limit(limit);
+    const products = await query.skip(skip).limit(limit).exec();
 
-    res
-      .status(200)
-      .json({
-        message: "All products data fetched successfully",
-        currentPage: page,
-        totalPages: totalPages,
-        totalProducts,
-        products: products,
-      });
+    res.status(200).json({
+      message: "All products data fetched successfully",
+      currentPage: page,
+      totalPages: totalPages,
+      totalProducts,
+      products: products,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
